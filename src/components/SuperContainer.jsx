ea084234eee0d793b61958d5f8f083df32d9f862
geomer simanca <simancanavarrogeomerandres@gmail.com>
2025-11-27 15:12:26 -0500

    se agrego una mayuscula porque habia un error

--- src/components/SuperContainer.jsx
+++ src/components/SuperContainer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from '../styles/style-supercontainer.module.scss'
+import style from '../styles/style-superContainer.module.scss'
 import Navbar from './Navbar'
 import ContanerPages from './ContanerPages'
 
